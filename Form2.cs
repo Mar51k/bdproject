8dcc0d70c60b4b4d37643bb305a81e19d3713836
Mar51k <dima.kretinin.05@mail.ru>
2024-06-10 22:24:43 +0700

    открыл подключение к бд для второй формы и переименовал все компоненты, для удобства в написании запроса

--- Form2.cs
+++ Form2.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Data.Sql;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace bdproject
 {
     public partial class Form2 : Form
     {
+        private SqlConnection conn = null;
         public Form2()
         {
             InitializeComponent();
             this.BackColor = Color.FromArgb(51, 51, 51);
             dataGridView1.BackgroundColor = Color.FromArgb(51, 51, 51);
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Data Source=DESKTOP-E9RKKNE\\SQLEXPRESS;Initial Catalog=&quot;airport 23.106 kretinin mamaev&quot;;Integrated Security=True;Encrypt=False"].ConnectionString);
+            conn.Open();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -31,6 +37,11 @@ namespace bdproject
         }
 
         private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void seacrhbutton_Click(object sender, EventArgs e)
         {
 
         }
